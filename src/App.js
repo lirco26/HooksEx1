3835408ac40805634376daec3ba878d49ebe9903
Lir <lircohen26@gmail.com>
2020-03-25 16:55:35 +0200

    added text input that shows the content below it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import TextDuplicator from './TextDuplicator.jsx';
 
 function App() {
     return (
         <div className="App">
-
+            <h1>Hooks Ex</h1>
+            <h2>(c) made by Lirco</h2>
+        <TextDuplicator/>
         </div>
     );
 }
---
+++ src/TextDuplicator.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+export default function TextDuplicator() {
+    const [content, setContent] = useState('');
+    return <div className="text-duplicator">
+        <input type="text" onChange={(event) => {
+            setContent(event.target.value);
+        }
+        }/>
+        <span>{content}</span>
+    </div>;
+}
